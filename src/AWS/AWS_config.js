720b4386f8a082aeb6824954d9153b72e9f8eaba
tonychen <trithong12@gmail.com>
2018-09-10 13:50:13 +0800

    test gitignore

---
+++ src/AWS/AWS_config.js
@@ -0,0 +1,10 @@
+const AWS = require('aws-sdk/dist/aws-sdk-react-native'); 
+AWS.config.update({
+    region: "ap-northeast-1",
+    credentials: {
+        accessKeyId: "*",
+        secretAccessKey: "*"
+    }
+});
+//AWS.config.loadFromPath('/Users/tonychen/AWS/config.json');
+export default AWS;
---
+++ src/AWS/cognito_config.js
@@ -0,0 +1,9 @@
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+
+const poolData = {
+    UserPoolId : '*', // your user pool id here
+    ClientId : '*' // your app client id here
+};
+const userPool = new CognitoUserPool(poolData);
+
+export default userPool;
