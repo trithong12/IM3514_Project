9133366b70cea65a3802cd3f9d01e6be71e8f132
tonychen <trithong12@gmail.com>
2018-09-10 14:06:24 +0800

    test gitignore

--- src/AWS/cognito_config.js
+++ src/AWS/cognito_config.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
-    UserPoolId : '*', // your user pool id here
-    ClientId : '*' // your app client id here
+    UserPoolId : '**', // your user pool id here
+    ClientId : '**' // your app client id here
 };
 const userPool = new CognitoUserPool(poolData);
 
