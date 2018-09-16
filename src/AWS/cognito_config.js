591d196203beead8cc2406c9f7eb95c5f8babf4a
trithong12 <trithong12@gmail.com>
2018-09-16 14:55:27 +0800

    Update cognito_config.js

--- src/AWS/cognito_config.js
+++ src/AWS/cognito_config.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
-    UserPoolId: 'ap-northeast-1_O1bojcWN7',
-    ClientId: '7k7s8v9ockt6ih2bucl2ejth3p'
+    UserPoolId: '＊＊＊＊＊',
+    ClientId: '＊＊＊＊＊'
 };
 const userPool = new CognitoUserPool(poolData);
 
